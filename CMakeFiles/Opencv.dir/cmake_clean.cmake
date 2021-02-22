806832bec30f4dbec5cf705db8165730cccfa966
HIMMEL95 <bbluesky7738@gmail.com>
2021-02-22 15:48:27 +0900

    first commit

---
+++ CMakeFiles/Opencv.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Opencv.dir/main.cpp.o"
+  "Opencv.pdb"
+  "Opencv"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Opencv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
